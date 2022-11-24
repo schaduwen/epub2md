d60752aefb837d980c0e0d3f7f3b1b025c0bec7e
schaduwen <schaduwen@protonmail.com>
2022-11-25 05:26:16 +1300

    Update epub2md.py
    metadata ini file is created, directory name no longer depends on epub file name, recycled filename variable - now chapname can be edited in future

--- epub2md.py
+++ epub2md.py
@@ -1,8 +1,12 @@
 from sys import argv
 from os import path, mkdir
+from configparser import ConfigParser
 from ebooklib import epub
+from bs4 import BeautifulSoup
 from markdownify import markdownify
 
+SECTION_NAME = 'METADATA'
+EXCLUDE = ['introduction.xhtml', 'nav.xhtml']
 
 if len(argv) <= 1:
     raise SystemExit()
@@ -16,15 +20,30 @@ if path.splitext(filename)[1] != '.epub':
     raise SystemExit()
 
 _epub = epub.read_epub(filename)
-xhtmls = _epub.get_items_of_media_type('application/xhtml+xml')
+html = _epub.get_item_with_href('introduction.xhtml').get_body_content()
 
-dirname = path.splitext(path.basename(filename))[0]
+ini = ConfigParser()
+ini.add_section(SECTION_NAME)
+for i, tag in enumerate(BeautifulSoup(html, 'html.parser')):
+    if i == 0:
+        booktitle = tag.text
+        ini.set(SECTION_NAME, 'title', booktitle)
+    elif i == 2:
+        bookauthor = tag.text.replace('By: ', '')
+        ini.set(SECTION_NAME, 'author', bookauthor)
+
+dirname = '{} - {}'.format(bookauthor, booktitle)
 
 if path.exists(dirname) is False:
     mkdir(dirname)
 
+with open('{}/metadata.ini'.format(dirname), 'w') as f:
+    ini.write(f)
+
+xhtmls = _epub.get_items_of_media_type('application/xhtml+xml')
 for xhtml in xhtmls:
-    chapname = path.splitext(xhtml.get_name())[0]
-    if chapname != 'nav':
-        with open('./{}/{}.md'.format(dirname, chapname), 'w') as f:
+    filename = xhtml.get_name()
+    if filename not in EXCLUDE:
+        chapname = path.splitext(filename)[0]
+        with open('{}/{}.md'.format(dirname, chapname), 'w') as f:
             f.write(markdownify(xhtml.get_body_content()))
