1c526d1d876d711f560f6701f3700e925a1e5adb
schaduwen <schaduwen@protonmail.com>
2022-12-02 01:18:13 +1300

    Update epub2md.py
    removed ini file generation, assume all epub has Dublin Core creator and title metadata to retrieve from, included a DRY_RUN option (similar to patch), changed imports

--- epub2md.py
+++ epub2md.py
@@ -1,49 +1,36 @@
 from sys import argv
-from os import path, mkdir
-from configparser import ConfigParser
-from ebooklib import epub
-from bs4 import BeautifulSoup
+from os.path import isfile, splitext, exists
+from os import mkdir
+from ebooklib.epub import read_epub
 from markdownify import markdownify
 
-SECTION_NAME = 'METADATA'
-EXCLUDE = ['introduction.xhtml', 'nav.xhtml']
+DRY_RUN = 1
 
 if len(argv) <= 1:
     raise SystemExit()
 
 filename = argv[1]
 
-if path.isfile(filename) is False:
+if not isfile(filename):
     raise SystemExit()
 
-if path.splitext(filename)[1] != '.epub':
+if splitext(filename)[1] != '.epub':
     raise SystemExit()
 
-_epub = epub.read_epub(filename)
-html = _epub.get_item_with_href('introduction.xhtml').get_body_content()
+_epub = read_epub(filename)
 
-ini = ConfigParser()
-ini.add_section(SECTION_NAME)
-for i, tag in enumerate(BeautifulSoup(html, 'html.parser')):
-    if i == 0:
-        booktitle = tag.text
-        ini.set(SECTION_NAME, 'title', booktitle)
-    elif i == 2:
-        bookauthor = tag.text.replace('By: ', '')
-        ini.set(SECTION_NAME, 'author', bookauthor)
+dirname = '{} - {}'.format(
+        _epub.get_metadata('DC', 'creator')[0][0],
+        _epub.get_metadata('DC', 'title')[0][0])
 
-dirname = '{} - {}'.format(bookauthor, booktitle)
+if not exists(dirname):
+    print('mkdir "{}"'.format(dirname))
+    if not DRY_RUN:
+        mkdir(dirname)
 
-if path.exists(dirname) is False:
-    mkdir(dirname)
-
-with open('{}/metadata.ini'.format(dirname), 'w') as f:
-    ini.write(f)
-
-xhtmls = _epub.get_items_of_media_type('application/xhtml+xml')
-for xhtml in xhtmls:
-    filename = xhtml.get_name()
-    if filename not in EXCLUDE:
-        chapname = path.splitext(filename)[0]
+for xhtml in _epub.get_items_of_media_type('application/xhtml+xml'):
+    chapname = splitext(xhtml.get_name())[0]
+    print('write "{}/{}.md"'.format(dirname, chapname))
+    if not DRY_RUN:
         with open('{}/{}.md'.format(dirname, chapname), 'w') as f:
             f.write(markdownify(xhtml.get_body_content()))
